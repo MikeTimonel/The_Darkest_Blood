e24e0359eaacd85338ee38ffdd47a79b01ca41a0
AndresFJimenez <andresito_s2@live.com>
2022-05-29 16:39:47 -0500

    Menu 1.0

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] float velocity;
+    [SerializeField] float jump;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float dirX = Input.GetAxisRaw("Horizontal");
+            rb.velocity = new Vector2(dirX * velocity, rb.velocity.y);
+            
+        if (Input.GetKeyDown("space")) {
+            rb.velocity = new Vector2(rb.velocity.x, jump);
+        }
+    }
+}
