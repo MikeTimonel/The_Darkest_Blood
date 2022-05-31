d503a8c14083c5b37eb8315914d995f4e5a5e2ba
MikeTimonel <migueltimotexd@gmail.com>
2022-05-31 17:17:47 -0500

    Inicio de tutorial

---
+++ Assets/Camara/Camara.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    public GameObject jugador;
+    public Vector2 minimo;
+    public Vector2 maximo;
+    public float suavizado;
+    Vector2 velocidad;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x, jugador.transform.position.x, ref velocidad.x, suavizado);
+        float posY = Mathf.SmoothDamp(transform.position.y, jugador.transform.position.y, ref velocidad.y, suavizado);
+
+        transform.position = new Vector3(Mathf.Clamp(posX, minimo.x, maximo.x), Mathf.Clamp(posY, minimo.y, maximo.y), transform.position.z);
+    }
+}
--- Assets/Tutorial/Fondo/Fondo_Movimiento.cs
+++ Assets/Tutorial/Fondo/Fondo_Movimiento.cs
@@ -8,14 +8,15 @@ public class Fondo_Movimiento : MonoBehaviour
     [SerializeField] private Vector2 velocidadMovimiento;
     private Vector2 offset;
     private Material material;
-
+    private Rigidbody2D jugadorRB;
     private void Awake()
     {
         material = GetComponent<SpriteRenderer>().material;
+        jugadorRB = GameObject.FindGameObjectWithTag("Prota").GetComponent<Rigidbody2D>();
     }
     private void Update()
     {
-        offset = velocidadMovimiento * Time.deltaTime;
+        offset = (jugadorRB.velocity.x/10f) *velocidadMovimiento * Time.deltaTime;
         material.mainTextureOffset += offset;
     }
 }
