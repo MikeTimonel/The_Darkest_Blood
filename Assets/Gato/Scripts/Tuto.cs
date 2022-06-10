1e5cbddf3d6adf10c7f0125aafb4b96eee94e3d6
MikeTimonel <migueltimotexd@gmail.com>
2022-06-10 13:18:29 -0500

    Casi terminado

--- Assets/Gato/Scripts/Tuto.cs
+++ Assets/Gato/Scripts/Tuto.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class Tuto : MonoBehaviour
 {
-    public Animator animadorgato;
-    public PlayerMovement protagonistascript;
-
+    public GameObject gatito;
 
     void FixedUpdate()
     {
@@ -14,46 +12,9 @@ public class Tuto : MonoBehaviour
     }
     void OnCollisionEnter2D(Collision2D collider)
     {
-        if (collider.gameObject.name == "Numero1")
+        if (collider.gameObject.name == "Protagonista")
         {
-            animadorgato.SetInteger("Tuto", 1);
-
+            gatito.SetActive(true);
         }
-        else if (collider.gameObject.name == "Numero2")
-        {
-            
-        }
-        else if (collider.gameObject.name == "Numero3")
-        {
-
-        }
-        else if (collider.gameObject.name == "Numero4")
-        {
-
-        }
-        else if (collider.gameObject.name == "Numero5")
-        {
-
-        }
-        else if (collider.gameObject.name == "Numero6")
-        {
-
-        }
-        else if (collider.gameObject.name == "Numero7")
-        {
-
-        }
-        else if (collider.gameObject.name == "Numero8")
-        {
-
-        }
-        else if (collider.gameObject.name == "Numero9")
-        {
-
-        }
-    }
-    private IEnumerator rutina1()
-    {
-        yield return new WaitForSeconds(5f);
     }
 }
